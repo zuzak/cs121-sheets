6d1bed2ef922eeb882856c31d606666cfed2af0b
Douglas Gardner <douglas@chippy.ch>
2012-12-01 19:10:22 +0000

    add string parser

---
+++ java/j02/StringParse.java
@@ -0,0 +1,9 @@
+public class StringParse {
+	public static void main(String args[]){
+		String subject = args[0];
+		System.out.println(subject.toUpperCase());
+		System.out.println(subject.toLowerCase());
+		System.out.println(subject.substring(1));
+//		System.out.println(subject.substring(-1));
+	}
+}
