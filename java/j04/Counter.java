eab0718c78064561c8e01665a8acdaa0902a64a1
Douglas Gardner <douglas@chippy.ch>
2012-12-03 10:12:42 +0000

    herp derp look at me my code works and shit

--- java/j04/Counter.java
+++ java/j04/Counter.java
@@ -1,9 +1,8 @@
 public class Counter {
 	private int count = 0;
-	int MaxCount = 9001; // final this
+	final int maxCount = 1; // final this
 	public void incrementCount(){
 		count++;
-		System.out.println("Incremented");
 		if (count > maxCount) {
 			resetCount();
 		}
@@ -13,7 +12,6 @@ public class Counter {
 	}
 	public void resetCount(){
 		count = 0;
-		System.out.println("Reset")
 	}
 	/*public void main(String args[]){
 		incrementCount();
